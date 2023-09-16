53a187a6d55e41f618a4c432dcfc8199f82a1a63
Teoman Selcuk <teomanege.selcuk@gmail.com>
2023-09-16 15:28:24 -0400

    New Dockefile from scratch and Archived old one

--- src/q1.lisp
+++ src/q1.lisp
@@ -1,1 +1,7 @@
-(defun square (x) (*x x))
+(defun square (x)
+  (* x x))
+
+(defun main (arg)
+  (format t "The square of ~a is ~a" arg (square arg)))
+
+(main 5) ; Replace 5 with the value you want to pass
